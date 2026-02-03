da978a5ba8e76d4e2e2298e68d23886469aead76
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-02-03 13:35:39 +0200

    fix: update base path in Vite configuration for forms directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/~ilkkamtk/hybrid26/forms/',
   test: {
     environment: 'jsdom',
     globals: true,
